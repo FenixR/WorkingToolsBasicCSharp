b4f3f8e5f81c5733454af723dac9a15eb9056aa8
FenixR <dlar2509@gmail.com>
2016-10-09 18:08:42 -0400

    Project Startup

---
+++ FenixR_CSharp_Working_Tools/FenixR_CSharp_Working_Tools/Database_Connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FenixR_CSharp_Working_Tools
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Database_Connection
+    {
+
+        public enum Database_Type
+        {
+           Error = 0,
+           SqlServer = 1
+        };
+
+    }
+}
